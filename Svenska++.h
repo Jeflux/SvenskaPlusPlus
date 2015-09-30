94fcf5f9021dd4677e27f8d1807a93e036be4649
Jeflux <jespermathiason@gmail.com>
2015-09-30 10:43:44 +0200

    Här kommer filen!

---
+++ Svenska++.h
@@ -0,0 +1,30 @@
+#ifndef SVENSKA_H
+#define SVENSKA_H
+
+#define klass class
+#define allmän public
+#define privat private
+#define skyddad protected
+#define virtuell virtual
+
+#define dubbel double
+#define flyt float
+#define heltal int
+#define inget void
+
+#define för for
+#define medans while
+#define gör do
+
+#define radera delete
+#define använder using
+
+#define nollpekare nullptr
+#define återvänd return
+#define dethär this
+#define automatisk auto
+
+#define använder using
+#define namnrymden namespace
+
+#endif
